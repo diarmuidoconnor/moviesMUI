a0559e0ac12bd2f426f9a36776c77bc3bbafb60d
Diarmuid O Connor <doconnor@wit.ie>
2021-01-17 16:53:33 +0000

    Move all Http code to single module

---
+++ src/api/tmdb-api.js
@@ -0,0 +1,39 @@
+export const getMovies = () => {
+  return fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      // console.log(json)
+      return json.results;
+    });
+};
+
+export const getMovie = (id) => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  ).then((res) => {
+    return res.json();
+  });
+};
+
+export const getGenres = () => {
+  return fetch(
+    "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
+      process.env.REACT_APP_TMDB_KEY +
+      "&language=en-US"
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      // console.log(json)
+      return json.genres;
+    });
+};
+
+export const getMovieImages = (id) => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
+  )
+    .then((res) => res.json())
+    .then((json) => json.posters);
+};
