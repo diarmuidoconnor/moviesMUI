50fecdbb6d424ba5a8bce86ce1cf0c82bd506edf
Diarmuid O Connor <doconnor@wit.ie>
2021-01-30 12:04:55 +0000

    Add upcoming movies page

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -48,3 +48,11 @@ export const getMovieReviews = (id) => {
       return json.results;
     });
 };
+
+export const getUpcomingMovies = () => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+  )
+    .then((res) => res.json())
+    .then((json) => json.results);
+};
--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
-import FavoriteMoviesPage from "./pages/favoriteMoviesPage"; // NEW
+import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage"; 
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from "./components/siteHeader";
 const App = () => {
   return (
     <BrowserRouter>
-      <SiteHeader />``
+      <SiteHeader />
+      ``
       <Switch>
         <Route path="/reviews/:id" component={MovieReviewPage} />
+        <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
         <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
         <Route path="/movies/:id" component={MoviePage} />
         <Route exact path="/" component={HomePage} />
