aca63305566a0090273c1e410e2de11c28b212ba
Diarmuid O Connor <doconnor@wit.ie>
2021-01-30 12:05:32 +0000

    Clean up home page

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -6,23 +6,6 @@ const HomePage = (props) => {
   const [movies, setMovies] = useState([]);
   const favorites = movies.filter(m => m.favorite)
   localStorage.setItem('favorites', JSON.stringify(favorites))
-  // const [nameFilter, setNameFilter] = useState("");
-  // const [genreFilter, setGenreFilter] = useState("0");
-
-  // const genre = Number(genreFilter);
-
-  // let displayedMovies = movies
-  //   .filter((m) => {
-  //     return m.title.toLowerCase().search(nameFilter.toLowerCase()) !== -1;
-  //   })
-  //   .filter((m) => {
-  //     return genre > 0 ? m.genre_ids.includes(Number(genreFilter)) : true;
-  //   });
-
-  // const handleChange = (type, value) => {
-  //   if (type === "name") setNameFilter(value);
-  //   else setGenreFilter(value);
-  // };
 
   const addToFavorites = (movieId) => {
     const updatedMovies = movies.map((m) =>
@@ -43,22 +26,7 @@ const HomePage = (props) => {
     movies={movies}
     selectFavorite={addToFavorites}
   />
-  //   <Grid container className={classes.root}>
-  //     <Grid item xs={12}>
-  //       <Header title={"Home Page"} />
-  //     </Grid>
-  //     <Grid item container spacing={5}>
-  //       <Grid key="find" item xs={12} sm={6} md={4} lg={3} xl={2}>
-  //         {/* <FilterCard /> */}
-  //         <FilterCard
-  //           onUserInput={handleChange}
-  //           titleFilter={nameFilter}
-  //           genreFilter={genreFilter}
-  //         />
-  //       </Grid>
-  //       <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
-  //     </Grid>
-  //   </Grid>
   );
 };
+
 export default HomePage;
