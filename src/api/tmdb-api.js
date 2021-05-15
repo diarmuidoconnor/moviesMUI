49146e708cb23a86f58adbc2aa9afd135303a1cc
Diarmuid O Connor <doconnor@wit.ie>
2021-05-15 09:29:03 +0100

    Disable some linting warnings

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -8,9 +8,10 @@ export const getMovies = async () => {
   return response.json();
 };
 
-export const getMovie = async ({ queryKey }) => {
+export const getMovie = async ( args ) => {
+  // console.log(args)
   // eslint-disable-next-line no-unused-vars
-  const [__key, { id }] = queryKey;
+  const [__key, { id }] = args.queryKey;
   const response = await fetch(
     `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
   );
@@ -33,6 +34,7 @@ export const getGenres = async () => {
 };
 
 export const getMovieImages = async ({queryKey}) => {
+    // eslint-disable-next-line no-unused-vars
   const [__key, { id }] = queryKey;
   const response = await fetch(
     `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
